8f0c68b4aab5543dbc3d4184d70d0362aa58757f
ntagungira-cmd <ntagungiraali@gmail.com>
2022-04-26 21:08:48 +0200

    first commit

---
+++ src/main/java/com/ntagungira/app/ws/sevice/UserService.java
@@ -0,0 +1,7 @@
+package com.ntagungira.app.ws.sevice;
+
+import com.ntagungira.app.ws.shared.dto.UserDto;
+
+public interface UserService {
+UserDto createUser(UserDto user);
+}
---
+++ src/main/java/com/ntagungira/app/ws/sevice/implementation/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.ntagungira.app.ws.sevice.implementation;
+
+import org.springframework.stereotype.Service;
+
+import com.ntagungira.app.ws.sevice.UserService;
+import com.ntagungira.app.ws.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserDto createUser(UserDto user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
