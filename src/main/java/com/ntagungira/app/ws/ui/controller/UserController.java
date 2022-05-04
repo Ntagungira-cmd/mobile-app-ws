5a493586d3cd348cfd7c297452d015257bfa1c82
ntagungira-cmd <ntagungiraali@gmail.com>
2022-05-04 21:51:47 +0200

    adding xml support

--- src/main/java/com/ntagungira/app/ws/ui/controller/UserController.java
+++ src/main/java/com/ntagungira/app/ws/ui/controller/UserController.java
@@ -2,6 +2,7 @@
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,7 @@ public class UserController {
 	@Autowired
 	UserService userService;
 
-	@GetMapping(path="/{id}")
+	@GetMapping(path="/{id}",produces=MediaType.APPLICATION_XML_VALUE)
 	public UserResp getUser(@PathVariable String id) {
 		UserResp returnValue=new UserResp();
 		UserDto userDto = userService.getUserByUserId(id);
